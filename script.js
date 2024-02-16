6557a007432d65854b44ac3d22e9626b87e58d25
AYESHAJUNAID <abdulhajunaid.com>
2024-02-16 21:03:44 +0500

    first commit

---
+++ script.js
@@ -0,0 +1,91 @@
+// 1st point
+alert("hello");
+
+// 2nd point
+alert("error ! please enter a valid passward");
+
+// 3rd point
+alert("welcome to JS land...\n happy coding!");
+
+// 4th point
+alert("Welcome to JS land..");
+alert("happy coding!");
+
+// 5th point
+alert("hello... I can run JS through my web browser's console");
+
+// chapter 2
+// 1st point
+var userName = "jhone";
+alert(userName);
+
+// 2nd point
+var myName = "jhone doe";
+alert(myName);
+
+// 3rd point
+var message = "hello world";
+alert(message);
+
+// 4th point
+var age = "15 Year Old";
+alert(age);
+var data = "certified mobile application development";
+alert(data);
+
+// 5th point
+var pizza = " PIZZA \n PIZZ \n PIZ \n PI \n P \n";
+
+alert(pizza);
+
+// 6th point
+var email = "example@example.com";
+
+alert("My email address is" + email);
+
+// 7th point
+var book = "A smarter way to learn JAVASCRIPT";
+
+alert("I am trying to learn from the book " + book);
+
+// 8th point
+document.write("Yah! I can write HTML content through JAVASCRIPT <br>");
+
+// 9th point
+var design = "“▬▬▬▬▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬▬▬▬▬”";
+
+alert(design);
+document.write(design)
+// 3rd chapter
+var age = " i am 15 years old "
+
+alert(age);
+// 2nd point
+var visitors = "you have visted this site 14 times";
+alert(visitors );
+
+// 3rd point
+var birthYear = "1990"
+document.write ("my birth year is " +birthYear+ "<br>")
+
+// 4th point
+
+var visitorsName = "jhone doe"
+var product ="XYZ clothing store"
+var quantity = "5 t-shirt";
+document.write(visitorsName+ " ordered " +quantity+ " (s) on " +product  )
+// chap 4
+var var1, var2, var3;
+// legalname
+var $dollar;
+var _underScore ;
+var variable ;
+var var1;
+var itsVariable;
+
+// illegal variable
+// var Username;
+// var 4number;
+// var -subtract;
+// var +add;
+// var (brackets)
